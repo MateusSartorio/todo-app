cb40fedbe1eb9b620d009ef93e451a12c1ef1949
MateusSartorio <mateussartorio39@gmail.com>
2023-12-22 16:00:09 -0300

    tasks can now be created and deleted

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:todo_app/tiles/create_task_dialog_box.dart';
 import 'package:todo_app/tiles/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController _controller = TextEditingController();
+
   List todoList = [
     ["Make tutorial", false],
     ["To exercise", false],
   ];
+
+  void saveNewTask() {
+    setState(() {
+      todoList.add([_controller.text, false]);
+      _controller.clear();
+    });
+
+    Navigator.of(context).pop();
+  }
 
   void checkboxChanged(bool? value, int index) {
     setState(() {
@@ -20,16 +33,40 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void createNewTask() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return CreateTaskDialogBox(
+          controller: _controller,
+          onSave: saveNewTask,
+          onCancel: () => {_controller.clear(), Navigator.of(context).pop()},
+        );
+      },
+    );
+  }
+
+  void deleteTask(int index) {
+    setState(() {
+      todoList.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("TODO")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: Icon(Icons.add),
+      ),
       body: ListView.builder(
           itemCount: todoList.length,
           itemBuilder: (context, index) => TodoTile(
                 taskName: todoList[index][0],
                 taskCompleted: todoList[index][1],
                 onChanged: (value) => checkboxChanged(value, index),
+                deleteFunction: (context) => deleteTask(index),
               )),
     );
   }
